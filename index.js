c7efe1f8c36d31b6d987dad47a3a327732d6fd90
Luis Pabón <luis.pabon@coreos.com>
2017-05-27 14:02:37 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Import the interface to Tessel hardware
+var tessel = require('tessel');
+var express = require("express");
+var bodyParser =require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res) {
+	res.send("Suck my noots");
+});
+
+var server = app.listen(3000, function () {
+	console.log("listening on port %s...", server.address().port);
+});
+
+// Turn one of the LEDs on to start.
+tessel.led[2].on();
+
+// Blink!
+setInterval(function () {
+  tessel.led[2].toggle();
+  tessel.led[3].toggle();
+}, 100);
+
+console.log("I'm blinking! (Press CTRL + C to stop)");
